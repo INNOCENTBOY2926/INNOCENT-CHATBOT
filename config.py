3a7f3b26c90a583942f8185cfc9580728a73aac2
𝐈𝐍𝐍𝐎𝐂𝐄𝐍𝐓 𝐁𝐎𝐘 <155868717+INNOCENTBOY2926@users.noreply.github.com>
2024-12-31 23:18:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,19 +4,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "6435225"
+API_ID = "28916577"
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "24f26bdbcca723053f2bd09ad524f904"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 STRING1 = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "7745014754"))
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/surajislam/KING-CHATBOT")
+OWNER_ID = int(getenv("OWNER_ID", "6961211249"))
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/INNOCENTBOY2926/INNOCENT-CHATBOT")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
-SUPPORT_GRP = "MS_Music_official"
-UPDATE_CHNL = "ABOUT_NIGHT_SQUAD"
-OWNER_USERNAME = "II_SB_SIME_II"
+SUPPORT_GRP = "THE_FUCKER_BOTS_2926"
+UPDATE_CHNL = "THE_FUCKING_BOT_2926"
+OWNER_USERNAME = "its_innocent_boy_2926"
 # GIT TOKEN ( if your edited repo is private)
 GIT_TOKEN = getenv("GIT_TOKEN", "")
     
